148a4d5fd4b59c2ef957b55156cbb2789c631590
Ivan Pozdniakov <Vanjapozd@gmail.com>
2024-12-17 14:53:17 +0200

    regcom

--- datingsite/recommendations/views.py
+++ datingsite/recommendations/views.py
@@ -7,9 +7,12 @@ import json
 
 def recom(request):
     user = request.user
-    users = CustomUser.objects.exclude(id=request.user.id).filter(age__gte=user.age-2, age__lte=user.age+2)
+    # users = CustomUser.objects.exclude(id=request.user.id).filter(age__gte=user.age-2, age__lte=user.age+2)
+    users = CustomUser.objects.exclude(id=request.user.id)
+    rec_user = users.first()
     if request.user.is_authenticated:
-        return render(request, 'recommendations.html', {'rec_user': users[0]})
+        images = [photo.url for photo in [rec_user.photo1, rec_user.photo2, rec_user.photo3, rec_user.photo4, rec_user.photo5, rec_user.photo6] if photo]
+        return render(request, 'recommendations.html', {'rec_user': users[0], 'images': images})
     else:
         return redirect('login')
 
@@ -33,13 +36,17 @@ def handle_action(request):
         users = CustomUser.objects.exclude(id=request.user.id).filter(age__gte=user.age-2, age__lte=user.age+2)
         next_user = users.filter(id__gt=user_id).first()
         
-        if next_user:
+        if next_user:   
+
+            images = [photo.url for photo in [next_user.photo1, next_user.photo2, next_user.photo3, next_user.photo4, next_user.photo5, next_user.photo6] if photo]
+            
             response_data = {
                 'user_first_name': next_user.first_name,
                 'user_last_name': next_user.last_name,
                 'user_age': next_user.age,
                 'user_desc': next_user.description,
-                'user_id': next_user.id
+                'user_id': next_user.id,
+                'images': images
             }
         else:
             response_data = {
