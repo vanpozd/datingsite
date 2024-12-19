da66094c6c925acb1d81acf9e3da836f5b67461e
Ivan Pozdniakov <Vanjapozd@gmail.com>
2024-12-19 10:09:09 +0200

    regcom

--- datingsite/profiles/urls.py
+++ datingsite/profiles/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.userprofile, name='profile'),
     path('edit/', views.editprofile, name='editprofile'),
+    path('delete_image/<int:image_id>', views.delete_image, name='delete_image'),
+    path('upload_image/', views.upload_image, name='upload_image'),
 ]
--- datingsite/profiles/views.py
+++ datingsite/profiles/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+import json
 
 def userprofile(request):
 	if request.user.is_authenticated:
@@ -27,5 +29,53 @@ def editprofile(request):
 		else:
 			images = [photo for photo in [user.photo1, user.photo2, user.photo3, user.photo4, user.photo5, user.photo6] if photo]
 			return render(request, 'editprofile.html', {'user': user, 'images': images})
+	else:
+		return redirect('login')
+	
+
+def delete_image(request, image_id):
+	if request.user.is_authenticated:
+		user = request.user
+		if image_id == 1:
+			
+			user.photo1 = None
+		elif image_id == 2:
+			user.photo2 = None
+		elif image_id == 3:
+			user.photo3 = None
+		elif image_id == 4:
+			user.photo4 = None
+		elif image_id == 5:
+			user.photo5 = None
+		elif image_id == 6:
+			user.photo6 = None
+		user.save()
+		return redirect('editprofile')
+	else:
+		return redirect('login')
+	
+def upload_image(request):
+	if request.user.is_authenticated:
+		user = request.user
+		if request.method == 'POST':
+			data = json.loads(request.body)
+			photo = data.get('image')
+			image_id = int(data.get('image_counter'))
+			if image_id == 1:
+				user.photo1 = photo
+			elif image_id == 2:
+				user.photo2 = photo
+			elif image_id == 3:
+				user.photo3 = photo
+			elif image_id == 4:
+				user.photo4 = photo
+			elif image_id == 5:
+				user.photo5 = photo
+			elif image_id == 6:
+				user.photo6 = photo
+			user.save()
+			return redirect('editprofile')
+		else:
+			return redirect('editprofile')
 	else:
 		return redirect('login')
