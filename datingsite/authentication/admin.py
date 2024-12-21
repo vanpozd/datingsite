3943753328d7fab73d9453bd4d33f58adec84adb
Ivan Pozdniakov <Vanjapozd@gmail.com>
2024-12-21 08:49:47 +0200

    regcom

--- datingsite/authentication/admin.py
+++ datingsite/authentication/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id','username', 'first_name', 'last_name', 'created_at', 'updated_at']
+    list_filter = ['reported']
+
+    class Meta:
+        model = CustomUser
+
+admin.site.register(CustomUser, CustomUserAdmin)
